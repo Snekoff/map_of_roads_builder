f7e69124a3381cedc4cf3e0e8c9fd57dc849d8f8
Denys Piven <maxvel46@gmail.com>
2022-08-02 16:02:23 +0300

    added tests for vertice fixed vertice save and load functions

--- tests/edge_test.js
+++ tests/edge_test.js
@@ -15,7 +15,7 @@ describe('Edge test', function () {
         let obj = {};
         obj.vertices = [0, 1];
         obj.name = "test";
-        obj.id = 0;
+        obj.id = -1;
         obj.protectionAmount = 0;
         obj.level = 0;
         obj.isLoaded = true;
@@ -70,4 +70,51 @@ describe('Edge test', function () {
             savedObj = null;
         })
     });
+
+    describe('load', function () {
+        let savedObj;
+        beforeEach(function () {
+            savedObj = edge.save();
+            edge = null;
+            edge = Edge.load(savedObj);
+        })
+
+        it('should return same object...check type', function () {
+            assert.equal(edge.type, savedObj.type);
+        });
+
+        it('should return same object...check name', function () {
+            assert.equal(edge.name, savedObj.name);
+        });
+
+        it('should return same object...check id', function () {
+            assert.equal(edge.id, savedObj.id);
+        });
+
+        it('should return same object...check richness', function () {
+            assert.equal(edge.length, savedObj.length);
+        });
+
+        it('should return same object...check type', function () {
+            assert.equal(edge.vertices[0] === savedObj.vertices[0], edge.vertices[1] === savedObj.vertices[1]);
+        });
+
+        it('should return same object...check richness', function () {
+            assert.equal(edge.protectionAmount, savedObj.protectionAmount);
+        });
+
+        it('should return same object...check type', function () {
+            assert.equal(edge.level, savedObj.level);
+        });
+
+        it('should return same object...check richness', function () {
+            assert.equal(edge.isForVisualisation, savedObj.isForVisualisation);
+        });
+
+        afterEach(function () {
+            savedObj = null;
+        })
+    });
+
+
 });
